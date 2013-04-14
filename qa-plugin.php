e7ac1fcde55efe1e5291edf8c946bc6e9775e1cf
chejug <chejug@yahoo.com>
2013-04-14 14:41:03 -0300

    Create qa-plugin.php

---
+++ qa-plugin.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+  Plugin Name: qa_unicornify
+	Plugin URI: 
+	Plugin Description: Change the gravatar to unicornify
+	Plugin Version: 1.0
+	Plugin Date: 2013-04-13
+	Plugin Author: jugu	
+	Plugin Author URI: 
+	Plugin License: GPLv2
+	Plugin Minimum Question2Answer Version: 1.5
+	Plugin Update Check URI: 
+*/
+
+if (!defined('QA_VERSION')) { // don't allow this page to be requested directly from browser
+	header('Location: ../../');
+	exit;
+}
+
+qa_register_plugin_overrides('qa-unicornify-format.php');
+qa_register_plugin_overrides('qa-unicornify-user.php');
+qa_register_plugin_module('module', 'qa-unicornify-admin.php', 'qa_unicornify_admin', 'Unicornify Avatar');
